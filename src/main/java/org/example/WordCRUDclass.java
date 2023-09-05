40339742f2c0f6434680b5d158db544667a345d3
SungminLee <sungmin7955@handong.ac.kr>
2023-09-05 10:30:08 +0900

    second

---
+++ src/main/java/org/example/WordCRUDclass.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class WordCRUDclass {
+    public int add() {
+
+        return 0;
+    }
+    public int update(){
+        return 0;
+    }
+    public int delete(){
+        return 0;
+    }
+    public void select(Object j){
+
+    }
+}
